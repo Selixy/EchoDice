69c170a4d3b68074b4fdba1e7f29af53749b9245
Selixy <selenabouloizeau@gmail.com>
2025-07-27 23:40:43 +0200

    bake2

--- core/cpp/src/network/connect_offer.cpp
+++ core/cpp/src/network/connect_offer.cpp
@@ -1,3 +1,4 @@
+#include "info.hpp"
 #include "network/connect_offer.hpp"
 #include "network/peer_manager.hpp"
 #include <rtc/rtc.hpp>
@@ -7,6 +8,7 @@ namespace network {
 
 void connect_to_offer(PeerManager& manager, const std::string& id, const std::string& remoteSdp) {
     auto pc = std::make_shared<rtc::PeerConnection>();
+    const std::string id = gInfo.ID;
 
     pc->onLocalDescription([id](rtc::Description desc) {
         std::cout << "\n=== ANSWER [" << id << "] ===\n"
--- core/cpp/src/network/create_offer.cpp
+++ core/cpp/src/network/create_offer.cpp
@@ -1,3 +1,4 @@
+#include "info.hpp"
 #include "network/create_offer.hpp"
 #include "network/peer_manager.hpp"
 #include "network/sdp_codec.hpp" // <<== ajout
@@ -8,6 +9,7 @@ namespace network {
 
 void create_offer(PeerManager& manager, const std::string& id) {
     auto pc = std::make_shared<rtc::PeerConnection>();
+    const std::string id = gInfo.ID;
 
     pc->onLocalDescription([id](rtc::Description desc) {
         std::string raw = static_cast<std::string>(desc);
